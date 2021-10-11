0fc48a4334825a64dea874995d3afdef6fb94c8b
sena <43688294+katesenalee@users.noreply.github.com>
2021-10-11 12:07:36 +0800

    Initial Commit

---
+++ cats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cats
+//
+//  Created by Sena Lee on 10/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cats/catsApp.swift
@@ -0,0 +1,17 @@
+//
+//  catsApp.swift
+//  cats
+//
+//  Created by Sena Lee on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct catsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
