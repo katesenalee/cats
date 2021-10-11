a7aa77f9c248c1ad2050f2da870a6313945f92b6
sena <43688294+katesenalee@users.noreply.github.com>
2021-10-11 12:28:54 +0800

    made fact and photo view (beans)

---
+++ CatFactView.swift
@@ -0,0 +1,20 @@
+//
+//  CatFactView.swift
+//  cats
+//
+//  Created by Sena Lee on 10/11/21.
+//
+
+import SwiftUI
+
+struct CatFactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CatFactView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatFactView()
+    }
+}
---
+++ CatPhotoView.swift
@@ -0,0 +1,20 @@
+//
+//  CatPhotoView.swift
+//  cats
+//
+//  Created by Sena Lee on 10/11/21.
+//
+
+import SwiftUI
+
+struct CatPhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CatPhotoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatPhotoView()
+    }
+}
